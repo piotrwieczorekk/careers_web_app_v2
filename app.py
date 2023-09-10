a1881f5da690084ab29a785d963ba6c49aa24cf6
piotrwieczorekk <137555675+piotrwieczorekk@users.noreply.github.com>
2023-09-10 23:10:43 +0000

    updated api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,jsonify
-from database import engine, get_jobs_from_db
+from database import engine, get_jobs_from_db, load_job_from_db
 from sqlalchemy import text
 
 app = Flask(__name__)
@@ -10,6 +10,15 @@ def home():
   jobs = get_jobs_from_db()
   return render_template('home2.html', 
                          jobs=jobs)
+
+@app.route('/job/<id>')
+def job(id):
+  job = load_job_from_db(id)
+  if job:
+    return render_template('job_id_page.html', 
+                          job=job)
+  else:
+    return 'There is no such job id.',404
 
 @app.route('/about')
 def about_us():
--- database.py
+++ database.py
@@ -25,4 +25,16 @@ def get_jobs_from_db():
             row_dict = {col: val for col, val in zip(columns, row)}
             jobs.append(row_dict)  # Append each job to the list
         return jobs
-  
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id = :val"), {"val":id})
+    columns = result.keys()  # Get the column names
+    jobs = []
+    for row in result:
+      row_dict = {col: val for col, val in zip(columns, row)}
+      jobs.append(row_dict)  # Append each job to the list
+    if len(jobs) == 0:
+      return None
+    else:
+      return jobs[0]
